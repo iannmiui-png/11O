fa2c39570b8e579976f9c14b7bcd64fde92aab87
iannmiui-png <i.ann.miui@gmail.com>
2026-06-16 22:52:58 -0600

    Update and rename 11O(3).py to 11O3.py

--- 11O3.py
+++ 11O3.py
@@ -1,4 +1,3 @@
-
 import sys,math,base64
 S=lambda c:(lambda i:(i,i+1)if i<51 else(51,9999))(ord(c)-65)
 def d(s):
@@ -18,15 +17,12 @@ def to_hept(n):
  while n>0:
   n-=1;n,r0=divmod(n,27);r.append(d[r0])
  return"".join(reversed(r))
-
 Crystal="eD0yMyx5PTExLHJ1bGU9QjMvUzIzCjExYm8kNGJvM2JvNWJvM2JvJDNib2JvMTFib2JvJDNib2JvMTFib2JvJDJvYm9ib2I5b2JvYm9iMm8kMm9ibzE1Ym9iMm8kM2JvMTVibyQzYm9ibzRib2JvNGJvYm8kNGIybzRib2JvM2IybyQxM2JvJDEzYjIh"
 def Complete(C):
  print(to_hept(unary),"= 11")
  print(base64.b64decode(C).decode())
-
 Complete(Crystal)
 o=[]
 if f(love)==6417380798434:o.append("TRANS")
 if f(hello)==5025651466356:o.append("RIGHTS")
 if o:print(*o)
-
